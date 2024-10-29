f06caae66d461bef495e0ec98de648e56628b468
ani2002bar <anitu.atilia@gmail.com>
2024-10-29 18:20:10 -0300

    test6

--- tests/test-8.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('about:blank');
-  await page.goto('https://www.hugedomains.com/domain_profile.cfm?d=fpune.com');
-});
